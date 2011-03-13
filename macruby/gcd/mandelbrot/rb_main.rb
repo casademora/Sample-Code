0fbafff8dfed6361f0cdda492beebef8b06508be
Saul Mora <saul@casademora.com>
2011-03-13 14:11:00 -0600

    Added mandelbrot generator sample

---
+++ macruby/gcd/mandelbrot/rb_main.rb
@@ -0,0 +1,23 @@
+#
+# rb_main.rb
+# mandelbrot
+#
+# Created by Saul Mora on 3/8/11.
+# Copyright Magical Panda Software LLC 2011. All rights reserved.
+#
+
+# Loading the Cocoa framework. If you need to load more frameworks, you can
+# do that here too.
+framework 'Cocoa'
+
+# Loading all the Ruby project files.
+main = File.basename(__FILE__, File.extname(__FILE__))
+dir_path = NSBundle.mainBundle.resourcePath.fileSystemRepresentation
+Dir.glob(File.join(dir_path, '*.{rb,rbo}')).map { |x| File.basename(x, File.extname(x)) }.uniq.each do |path|
+  if path != main
+    require(path)
+  end
+end
+
+# Starting the Cocoa main loop.
+NSApplicationMain(0, nil)
