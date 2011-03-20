ed14f0bc8097a161071d1642d148c3dba1260fe9
Saul Mora <saul@casademora.com>
2011-03-20 12:59:50 +0900

    Moved to NSConf2011 folder Get macruby initial project working

---
+++ nsconf2011/CoreDataSample/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  CoreDataSample
+//
+//  Created by Saul Mora on 3/20/11.
+//  Copyright Magical Panda Software LLC 2011. All rights reserved.
+//
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+    return macruby_main("rb_main.rb", argc, argv);
+}
